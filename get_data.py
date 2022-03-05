de37b5c5a0caf7f2177185f85ea356ce8733d092
Neha Kennard <nnayak@cs.umass.edu>
2022-03-05 13:56:33 +0000

    i think this is getting reviews properly

--- get_data.py
+++ get_data.py
@@ -1,7 +1,13 @@
 import collections
 import json
 import openreview
+import os
+import stanza
 import tqdm
+
+STANZA_PIPELINE = stanza.Pipeline('en',
+                                  processors='tokenize,lemma,pos,depparse')
+
 
 def note_is_review(note):
   if not 'review' in note.content:
@@ -9,39 +15,80 @@ def note_is_review(note):
   assert len(note.signatures) == 1
   return "AnonReviewer" in note.signatures[0]
 
-Review = collections.namedtuple("Review",
-"forum_id review_id text confidence rating".split())
+
+ReviewMetadata = collections.namedtuple(
+    "ReviewMetadata", "forum_id review_id confidence rating".split())
+
+STANZA_KEYS = "text xpos head deprel".split()
+
+
+class Review(object):
+
+  def __init__(self, forum_id, review_id, text, confidence, rating):
+    self.metadata = ReviewMetadata(forum_id, review_id, confidence,
+                                   rating)._asdict()
+    self.text = text
+    self.syntax = self._get_syntax(text)
+
+  def _get_syntax(self, text):
+    doc = STANZA_PIPELINE(text)
+    sentences = []
+    for sentence in doc.sentences:
+      sentences.append(
+          [self._get_token_info(token) for token in sentence.tokens])
+    syntax = {}
+    for key in STANZA_KEYS:
+      syntax[key] = [[tok[key] for tok in sentence] for sentence in sentences]
+    return syntax
+
+  def _get_token_info(self, token):
+    token_dict, = token.to_dict()
+    return {key: token_dict[key] for key in STANZA_KEYS}
+
+  def as_json(self):
+    dict_prep = {"text" : self.text}
+    dict_prep.update(self.metadata)
+    dict_prep.update(self.syntax)
+    return json.dumps(dict_prep)
+
 
 def get_reviews(notes, forum_id):
   reviews = []
   for note in notes:
     if note_is_review(note) and note.replyto == forum_id:
-      reviews.append(Review(forum_id, note.id, note.content['review'],
-    note.content['confidence'], note.content['rating']))
+      reviews.append(
+          Review(forum_id, note.id, note.content['review'],
+                 note.content['confidence'], note.content['rating']))
+
   return reviews
+
+
+def save_reviews(forum_id, client, data_dir):
+  notes = client.get_notes(forum=forum_id)
+  for review in get_reviews(notes, forum_id):
+    review_dir = data_dir + "/" + forum_id
+    os.makedirs(review_dir, exist_ok=True)
+    with open(review_dir + "/" + review.metadata['review_id'] + ".json", 'w') as f:
+      f.write(review.as_json())
+
 
 def main():
   guest_client = openreview.Client(baseurl='https://api.openreview.net')
+  data_dir = "reviews/"
   offset = 0
-  blind_submissions = []
+  limit = 10
   while True:
-    some_blind_submissions = guest_client.get_notes(
-        invitation='ICLR.cc/2021/Conference/-/Blind_Submission', offset=offset)
-    if not some_blind_submissions:
+    print("Working on submissions {0} to {1}".format(offset, offset + limit))
+    blind_submissions = guest_client.get_notes(
+        invitation='ICLR.cc/2021/Conference/-/Blind_Submission',
+        offset=offset,
+        limit=limit)
+    for sub in tqdm.tqdm(blind_submissions):
+      save_reviews(sub.forum, guest_client, data_dir)
+    if blind_submissions:
+      offset += limit
+    else:
       break
-    blind_submissions += some_blind_submissions
-    offset += 1000
-  reviews = []
-  print("Getting reviews from {0} submissions".format(len(blind_submissions)))
-  for sub in tqdm.tqdm(blind_submissions):
-    forum_id = sub.forum
-    notes = guest_client.get_notes(forum=forum_id)
-    reviews += get_reviews(notes, forum_id)
-
-  with open('reviews.jsonl', 'w') as f:
-    for review in reviews:
-      f.write(json.dumps(review._asdict()) + "\n")
-
 
 
 if __name__ == "__main__":
