119b4f44c95f66d2ade6740a73f53f7a603c4c4f
nnayak_umass_edu <nnayak_umass_edu@ials-gpu006.unity.rc.umass.edu>
2022-05-22 18:05:24 +0000

    disapere prep done

--- similarity/prep_disapere_data.py
+++ similarity/prep_disapere_data.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import glob
 import json
@@ -5,50 +6,111 @@ import openreview
 import numpy as np
 import tqdm
 
-DISAPERE_ENV = "../data/DISAPERE/final_dataset/"
-SUBSETS = "train dev test".split()
 
-Example = collections.namedtuple("Example",
-  "review_sentences rebuttal_sentences alignment forum_id")
 
-GUEST_CLIENT = openreview.Client(baseurl='https://api.openreview.net')
+parser = argparse.ArgumentParser(
+    description='Prepare DISAPERE data for similarity calculations')
+parser.add_argument('-d', '--disapere_path', default="../data/DISAPERE/final_dataset/",
+    type=str, help='path to DISAPERE final_dataset directory')
+parser.add_argument('-p', '--pdf_dir', default="pdfs/",
+    type=str, help='path to directory where pdfs will be saved')
+parser.add_argument('-o', '--output_file', default="disapere_similarity_input.json",
+    type=str, help='output json filename')
+
+
+GUEST_CLIENT = openreview.Client(baseurl="https://api.openreview.net")
+Example = collections.namedtuple(
+    "Example",
+    "review_text reviewer_id rebuttal_text alignment forum_id review_id")
+
 
 def process_example(filename):
-  with open(filename, 'r') as f:
+  with open(filename, "r") as f:
     obj = json.load(f)
-    review_sentences = [sentence["text"] for sentence in obj["review_sentences"]]
-    rebuttal_sentences = [sentence["text"] for sentence in obj["rebuttal_sentences"]]
+    review_sentences = [
+        sentence["text"] for sentence in obj["review_sentences"]
+    ]
+    rebuttal_sentences = [
+        sentence["text"] for sentence in obj["rebuttal_sentences"]
+    ]
     alignment = np.zeros([len(rebuttal_sentences), len(review_sentences)])
-    for reb_i, sentence in enumerate(obj['rebuttal_sentences']):
-      align_type, indices = sentence['alignment']
+    for reb_i, sentence in enumerate(obj["rebuttal_sentences"]):
+      align_type, indices = sentence["alignment"]
       if indices is not None:
         for rev_i in indices:
           alignment[reb_i][rev_i] = 1
 
   return Example(
-    review_sentences, rebuttal_sentences, alignment.tolist(),
-    obj['metadata']['forum_id']
+      "\n\n".join(review_sentences),
+      obj["metadata"]["reviewer"],
+      "\n\n".join(rebuttal_sentences),
+      alignment.tolist(),
+      obj["metadata"]["forum_id"],
+      obj["metadata"]["review_id"],
   )
+
 
 def write_pdf(forum_id, pdf_directory):
   pdf_binary = GUEST_CLIENT.get_pdf(forum_id, is_reference=False)
-  with open(f'{pdf_directory}/{forum_id}.pdf', 'wb') as file_handle:
+  pdf_path = f"{pdf_directory}/{forum_id}.pdf"
+  with open(pdf_path, "wb") as file_handle:
     file_handle.write(pdf_binary)
+  return {
+      "forum_id": forum_id,
+      "manuscript_pdf_path": pdf_path,
+  }
+
+
+def build_structures(examples):
+  forum_to_reviews = collections.defaultdict(list)
+  review_map = {}
+  for example in examples:
+    forum_to_reviews[example.forum_id].append(example.review_id)
+    review_map[example.review_id] = example
+
+  structure_list = []
+  for forum, reviews in forum_to_reviews.items():
+    structure_builder = {"forum_id": forum, "reviews": []}
+    for review_id in reviews:
+      review = review_map[review_id]
+      assert review_id == review.review_id
+      structure_builder["reviews"].append({
+          "review_id": review.review_id,
+          "review_text": review.review_text,
+          "reviewer_id": review.reviewer_id,
+          "rebuttal_text": review.rebuttal_text,
+      })
+    structure_list.append(structure_builder)
+  return structure_list
+
 
 def main():
-  written_forums = []
-  examples = collections.defaultdict(list)
-  for subset in SUBSETS:
-    print(subset)
-    for filename in tqdm.tqdm(glob.glob(f'{DISAPERE_ENV}/{subset}/*')):
-      example = process_example(filename)
-      examples[subset].append(example._asdict())
-      if example.forum_id not in written_forums:
-        write_pdf(example.forum_id, 'pdfs/')
-        written_forums.append(example.forum_id)
 
-  with open('disapere_intermediate.json', 'w') as f:
-    json.dump({"pdf_dir":"pdfs/", "subsets":examples}, f)
+  args = parser.parse_args()
+
+  manuscript_list = []
+  pdfs_downloaded = []
+  examples = []
+  for filename in tqdm.tqdm(glob.glob(f"{args.disapere_path}/train/*")):
+    example = process_example(filename)
+    examples.append(example)
+    if example.forum_id not in pdfs_downloaded:
+      manuscript_list_item = write_pdf(example.forum_id, args.pdf_dir)
+      pdfs_downloaded.append(example.forum_id)
+      manuscript_list.append(manuscript_list_item)
+
+  structures = build_structures(examples)
+
+  with open(args.output_file, "w") as f:
+    json.dump(
+        {
+            "pdf_dir": args.pdf_dir,
+            "structures": build_structures(examples),
+            "manuscript_files": manuscript_list,
+        },
+        f,
+    )
+
 
 if __name__ == "__main__":
   main()
--- similarity/tokenize_pdfs.py
+++ similarity/tokenize_pdfs.py
@@ -5,8 +5,11 @@ import tqdm
 import xml.etree.ElementTree as ET
 
 import stanza
-STANZA_PIPELINE = stanza.Pipeline('en',
-                                  processors='tokenize',)
+
+STANZA_PIPELINE = stanza.Pipeline(
+    "en",
+    processors="tokenize",
+)
 
 PREFIX = "{http://www.tei-c.org/ns/1.0}"
 TEXT_ID = f"{PREFIX}text"
@@ -15,6 +18,7 @@ DIV_ID = f"{PREFIX}div"
 HEAD_ID = f"{PREFIX}head"
 P_ID = f"{PREFIX}p"
 
+
 def sentence_tokenize(text):
   return [sentence.text for sentence in STANZA_PIPELINE(text).sentences]
 
@@ -22,33 +26,33 @@ def sentence_tokenize(text):
 def get_docs(filename):
   section_titles = []
   section_texts = []
-  divs = ET.parse(filename).getroot().findall(
-    TEXT_ID)[0].findall(BODY_ID)[0].findall(DIV_ID)
+  divs = (ET.parse(filename).getroot().findall(TEXT_ID)[0].findall(BODY_ID)
+          [0].findall(DIV_ID))
   for div in divs:
-    header_node, = div.findall(HEAD_ID)
+    (header_node,) = div.findall(HEAD_ID)
     section_titles.append(header_node.text)
     text = ""
     for p in div.findall(P_ID):
       text += " ".join(p.itertext())
     section_texts.append(text)
-  return {k:v for k,v in zip(section_titles, section_texts)}
+  return {k: v for k, v in zip(section_titles, section_texts)}
 
 
 def main():
   tokenized_papers = {}
-  for filename in tqdm.tqdm(glob.glob('xmls/*.tei.xml')):
+  for filename in tqdm.tqdm(glob.glob("xmls/*.tei.xml")):
     forum_id = filename[5:-8]
     try:
       section_map = get_docs(filename)
       tokenized_papers[forum_id] = {
-        title: sentence_tokenize(text) for title, text in section_map.items()
+          title: sentence_tokenize(text) for title, text in section_map.items()
       }
     except ValueError:
       print(f"Problem with {filename}")
 
-  with open('disapere_tokenized_papers.json', 'w') as f:
+  with open("disapere_tokenized_papers.json", "w") as f:
     json.dump(tokenized_papers, f)
+
 
 if __name__ == "__main__":
   main()
-
