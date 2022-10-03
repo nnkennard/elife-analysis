b55a1d7cccc8c2df095ed6a239ea7d05e94837ef
Neha Kennard <nnayak@umass.edu>
2022-10-03 15:42:39 +0000

    before switching to sqlalchemy

--- graph/01_build_pubmed_graph.py
+++ graph/01_build_pubmed_graph.py
@@ -2,36 +2,80 @@ import csv
 import sqlite3
 
 
-# TODO(dss): what is the paper identifier in pubmed reference objects?
-class Paper(object):
-  IDENTIFIERS = "elife_manuscript_id pm_id doi issn".split()
-  USER_KEYS = (
-      IDENTIFIERS +
-      "title date_revised date_completed journal_pub_date journal".split())
-  # ALL_KEYS = "source_type paper_id".split() + USER_KEYS
+class Table(object):
+
+  def __init__(
+      self,
+      name,
+      non_identifier_user_keys,
+      primary_key=None,
+      foreign_keys=[],
+      identifiers=[],
+  ):
+    self.name = name
+    self.primary_key = primary_key
+    self.identifiers = identifiers
+    self.foreign_keys = foreign_keys
+    self.user_keys = identifiers + non_identifier_user_keys
 
 
-# TODO(dss): do we have any other person identifiers?
-class Person(object):
-  IDENTIFIERS = "elife_id orcid".split()
-  USER_KEYS = IDENTIFIERS
+PAPER = Table(
+    "paper",
+    "title date_revised date_completed journal_pub_date journal".split(),
+    primary_key="paper_id",
+    identifiers="elife_manuscript_id pm_id doi issn".split(),
+)
+
+PERSON = Table("person", [],
+               primary_key="person_id",
+               identifiers="elife_id orcid".split())
+
+AUTHORSHIP = Table("authorship", ["author_order"],
+                   foreign_keys="paper_id person_id".split())
+
+ALIAS = Table(
+    "alias",
+    "last_name first_name initials affiliation".split(),
+    foreign_keys="paper_id person_id".split(),
+)
 
 
-def check_for_any_keys(cursor, row, table, identifiers):
+def check_for_any_keys(cursor, row, table):
   key_string = " OR ".join([
-      f"{key} == {row[key]}" for key in identifiers if key in row and row[key]
+      f"{key} == {row[key]}" for key in table.IDENTIFIERS
+      if key in row and row[key]
   ])
-  found_counter = {}
-  maybe_matching_rows = cursor.execute(f"""
-    SELECT * FROM {table} WHERE {key_string}
+  maybe_matching_rows = cursor.execute(
+      f""" SELECT * FROM {table.NAME} WHERE {key_string}
   """).fetchall()
   assert len(maybe_matching_rows) in [0, 1]
   return maybe_matching_rows
 
 
+def check_for_key_conflicts(existing_table_row, new_row):
+  print(existing_table_row.keys(), new_row.keys())
+  # TODO(nnk): Clean this up with feedback from actual data
+  for key, value in row.items():
+    pass
+
+
+def insert_row(table, keyword_map, cursor):
+
+  sorted_keys, sorted_values = zip(*[(key, keyword_map[key])
+                                     for key in sorted(keyword_map)
+                                     if keyword_map[key]])
+
+  key_string = f"{table.PRIMARY_KEY}, " + ", ".join(sorted_keys)
+  value_string = "NULL," + ", ".join([f'"{v}"' for v in sorted_values])
+  print(f""" INSERT INTO {table.NAME}({key_string}) VALUES ({value_string});""")
+  k = cursor.execute(
+      f""" INSERT INTO {table.NAME}({key_string}) VALUES ({value_string});""")
+  (row,) = cursor.execute(f"""SELECT last_insert_rowid()""").fetchall()
+  return row[0]
+
+
 def insert_or_update_paper(cursor, paper_row):
-  maybe_matching_rows = check_for_any_keys(cursor, paper_row, "paper",
-                                           Paper.IDENTIFIERS)
+  maybe_matching_rows = check_for_any_keys(cursor, paper_row, Paper)
   if maybe_matching_rows:
     # Check for updates and/or conflicts
     (row,) = maybe_matching_rows
@@ -40,13 +84,9 @@ def insert_or_update_paper(cursor, paper_row):
     builder = {
         key: paper_row[key] for key in Paper.USER_KEYS if key in paper_row
     }
-    key_string = ", ".join(["source_type", "paper_id"] + sorted(builder.keys()))
-    value_string = f'"Pubmed", NULL,' + ", ".join(
-        f'"{builder[key]}"' for key in sorted(builder.keys()))
-    k = cursor.execute(
-        f""" INSERT INTO paper({key_string}) VALUES ({value_string});""")
-    (row,) = cursor.execute(f"""SELECT last_insert_rowid()""").fetchall()
-    return row[0]
+    builder["source_type"] = "PubMed"
+    new_row_id = insert_row(Paper, builder, cursor)
+    return new_row_id
 
 
 def insert_or_update_person(cursor, person_row):
@@ -60,23 +100,41 @@ def insert_or_update_person(cursor, person_row):
     builder = {
         key: person_row[key] for key in Person.USER_KEYS if key in person_row
     }
-    key_string = ", ".join(["person_id"] + sorted(builder.keys()))
-    value_string = f"NULL," + ", ".join(
-        f'"{builder[key]}"' for key in sorted(builder.keys()))
-    k = cursor.execute(
-        f""" INSERT INTO paper({key_string}) VALUES ({value_string});""")
-    (row,) = cursor.execute(f"""SELECT last_insert_rowid()""").fetchall()
-    return row[0]
+    new_row_id = insert_row(Person, builder, cursor)
+    return new_row_id
 
 
 def process_pubmed_citation(cursor, row, citer_id):
   citee_id = insert_or_update_paper(row)
-  # INSERT INTO citation VALUES citer_id, citee_id, PubMed
+  cursor.execute(
+      f""" INSERT INTO citation(citer_id, citee_id, source_type) VALUES
+      ({citer_id}, {citee_id}, "PubMed");""")
 
 
-def insert_or_update_alias(cursor, row, paper_id, author_i):
+def insert_or_update_alias(cursor, row, paper_id, author_order):
   person_id = insert_or_update_person(row)
-  # INSERT INTO authorship VALUES paper_id, person_id, author_order
+  insert_row(
+      Alias,
+      {
+          "paper_id": paper_id,
+          "person_id": person_id,
+          "author_order": str(author_order),
+      },
+      cursor,
+  )
+  insert_row(
+      Alias,
+      {
+          "paper_id": paper_id,
+          "person_id": person_id,
+          "author_order": str(author_order),
+      },
+      cursor,
+  )
+
+  cursor.execute(
+      f""" INSERT INTO alias(paper_id, person_id, author_order) VALUES
+      ({paper_id}, {person_id}, "{author_order}");""")
   # INSERT INTO alias VALUES paper_id, person_id, first_name, last_name, affiliation
   # ^ These should not lead to conflicts because this is the first time this pair is in here
 
@@ -84,9 +142,9 @@ def insert_or_update_alias(cursor, row, paper_id, author_i):
 def process_pubmed_row(cursor, row):
   paper_id = insert_or_update_paper(cursor, row)
   # for author_i, author in enumerate(row['authors']):
-  #  insert_or_update_alias(cursor, row, paper_id, author_i)
+  #   insert_or_update_alias(cursor, row, paper_id, author_i)
   # for reference in row['references']:
-  # process_pubmed_citations(reference)
+  #   process_pubmed_citations(reference, paper_id)
 
 
 def pubmed_row_generator():
