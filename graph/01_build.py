9c05487fffaa7e51a0ea0a255f699ee3c2a447ad
Neha Kennard <nnayak@umass.edu>
2022-10-04 18:57:52 +0000

    slight cleanup

--- graph/01_build.py
+++ graph/01_build.py
@@ -10,6 +10,18 @@ def pubmed_row_generator():
   with open("pubmed_data.tsv", "r") as f:
     pubmed_reader = csv.DictReader(f, delimiter='\t')
     for row in pubmed_reader:
+			# TODO(dss): before yielding, modify the keys to match the ones in paper
+      # and person keys. Row should be in the format:
+      # { key1: value1,
+      #   key2: value2,
+      #   ...
+      #   "authors": [author_dict_1, author_dict_2, ...]
+      #   "references": [reference_dict_1, reference_dict_2, ...]
+      # }
+			# Author dicts should be in the format
+			# { key1: value1,... } etc., but if there is no value provided for a
+			# field e.g. ORCID, just don't include the key in the dict.
+
       yield row
 
 def get_or_create_author_id(author_row, session):
@@ -39,12 +51,15 @@ def main():
 
   with Session(engine) as session:
     for pubmed_row in pubmed_row_generator():
+			# Create a row for this paper in the paper table
       paper = Paper(title=pubmed_row['title'])
       session.add(paper)
-      session.flush()
-      print(paper.id)
+      session.flush() # This allows us to access paper.id later
+
+			# Collect authorship and alias informationi
       authorships_and_aliases = []
       for i, author in enumerate(json.loads(pubmed_row['authors'])):
+				# The author may already exist as a person in the person table
         author_id = get_or_create_author_id(author, session)
         authorships_and_aliases.append(
         Authorship(person_id=author_id, paper_id=paper.id,
