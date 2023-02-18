27528a008288ce1abc9e35bd211256a1ec2f7e9e
Neha Kennard <nnayak@umass.edu>
2023-02-18 02:00:19 +0000

    clean version

--- data/convert_review_advisor.py
+++ data/convert_review_advisor.py
@@ -1,13 +1,31 @@
+import argparse
 import collections
 from interval import Interval
 import json
+import os
 import sys
 import stanza
 import tqdm
 
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('-i', '--input_file', default='', type=str, help='')
+parser.add_argument('-o', '--output_dir', default='', type=str, help='')
+
 SENTENCIZE_PIPELINE = stanza.Pipeline("en", processors="tokenize")
 TOLERANCE = 7
 
+label_map = {
+    "positive": "pol_positive",
+    "negative": "pol_negative",
+    "clarity": "asp_clarity",
+    "meaningful_comparison": "asp_meaningful-comparison",
+    "motivation": "asp_motivation-impact",
+    "originality": "asp_originality",
+    "replicability": "asp_replicability",
+    "soundness": "asp_soundness-correctness",
+    "substance": "asp_substance",
+}
 
 Sentence = collections.namedtuple("Sentence", "interval text")
 
@@ -36,7 +54,6 @@ def label_sentences(sentences, label_obj):
                 pass
             else:
                 overlap = sentence.interval & label_interval
-                print(overlap.upper_bound - overlap.lower_bound)
                 if overlap.upper_bound - overlap.lower_bound > TOLERANCE:
                     labels[i].append(label)
     return labels
@@ -64,8 +81,8 @@ def build_dict(review_id, sentence_idx, sentence, label_list):
         labels = {
             "review_action": "arg_evaluative",
             "fine_review_action": "none",
-            "aspect": aspect,
-            "polarity": polarity,
+            "aspect": label_map[aspect],
+            "polarity": label_map[polarity],
         }
     labels["sentence_index"] = sentence_idx
     labels["review_id"] = review_id
@@ -74,26 +91,29 @@ def build_dict(review_id, sentence_idx, sentence, label_list):
 
 
 def main():
-    with open(sys.argv[1], "r") as f:
-        lines = f.readlines()[:1000]
-        for line in tqdm.tqdm(lines):
-            obj = json.loads(line)
-            sentences = tokenize(obj["text"])
-            labels = label_sentences(sentences, obj["labels"])
-            sentence_dicts = []
-            for i, (sentence, label_list) in enumerate(zip(sentences, labels)):
-                sentence_dicts.append(build_dict(obj["id"], i, sentence, label_list))
+    args = parser.parse_args()
+    with open(args.input_file, "r") as f:
+        lines = f.readlines()
+        for offset in range(100):
+          for line in tqdm.tqdm(lines[offset::100]):
+              obj = json.loads(line)
+              sentences = tokenize(obj["text"])
+              labels = label_sentences(sentences, obj["labels"])
+              sentence_dicts = []
+              for i, (sentence, label_list) in enumerate(zip(sentences, labels)):
+                  sentence_dicts.append(build_dict(obj["id"], i, sentence, label_list))
 
-            with open(f'review_advisor_converted/{obj["id"]}.json', "w") as f:
-                json.dump(
-                    {
-                        "metadata": {
-                            "review_id": obj["id"],
-                        },
-                        "review_sentences": sentence_dicts,
-                    },
-                    f,
-                )
+              os.makedirs(f'{args.output_dir}/{offset}/', exist_ok=True)
+              with open(f'{args.output_dir}/{offset}/{obj["id"]}.json', "w") as f:
+                  json.dump(
+                      {
+                          "metadata": {
+                              "review_id": obj["id"],
+                          },
+                          "review_sentences": sentence_dicts,
+                      },
+                      f,
+                  )
 
 
 if __name__ == "__main__":
---
+++ who_wins/00_prep_data.sh
@@ -0,0 +1,24 @@
+
+# Download data
+
+# AMPERE
+wget https://xinyuhua.github.io/resources/naacl2019/naacl19_dataset.zip
+unzip naacl19_dataset.zip
+mkdir -p data/raw/ampere
+mv dataset/iclr_anno_final/* data/raw/ampere/
+rm naacl19_dataset.zip
+rm -r dataset/
+
+# DISAPERE
+wget https://github.com/nnkennard/DISAPERE/raw/main/DISAPERE.zip
+unzip DISAPERE.zip
+mkdir -p data/raw/disapere
+mv DISAPERE/final_dataset/* data/raw/disapere/
+rm -r DISAPERE*
+
+# ReviewAdvisor (has to be scp-ed)
+mkdir -p data/raw/revadv
+# scp -i ~/.ssh/id_nnk_courbet -r ~/Downloads/dataset-2/aspect_data nnayak_umass_edu@unity.rc.umass.edu:/work/nnayak_umass_edu/elife-analysis/who_wins/data/raw/revadv 
+
+
+# Convert data format
