e0c4e2eb014191bc47059a3216d3729086b48ad4
Neha Kennard <nnayak@umass.edu>
2023-05-09 18:54:59 +0000

    clean but doesn't do confidence

--- who_wins/04_analyze_results.py
+++ who_wins/04_analyze_results.py
@@ -28,74 +28,69 @@ parser.add_argument(
 
 Result = collections.namedtuple("Result", "precision recall fscore support".split())
 
+
 def modify_with_confidence(probabilities, positive_class, confidence_threshold):
-  labels = []
-  for p_array in probabilities:
-    #print("!!", p_array[positive_class])
-    if max(p_array) == p_array[positive_class]:
-      print("!!", p_array[positive_class])
-    if p_array[positive_class] > confidence_threshold and max(p_array) == p_array[positive_class]:
-      labels.append(1)
-    else:
-      labels.append(0)
-  return labels
+    labels = []
+    for p_array in probabilities:
+        if (
+            p_array[positive_class] > confidence_threshold
+            and max(p_array) == p_array[positive_class]
+        ):
+            labels.append(1)
+        else:
+            labels.append(0)
+    return labels
+
 
 def modify_to_binary(labels, positive_class):
-  return [
-    int(label == positive_class)  for label in labels
-  ]
+    return [int(label == positive_class) for label in labels]
+
+
+ResultFile = collections.namedtuple(
+    "ResultFile", "predictions true_labels probabilities label_names"
+)
+
+
+def read_result_file(filename):
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        label_names = reader.fieldnames[3:]
+        rows = [r for r in reader]
+    predictions = [int(row["label"]) for row in rows]
+    true_labels = [int(row["true_label"]) for row in rows]
+    probabilities = [[float(row[name]) for name in label_names] for row in rows]
+
+    return ResultFile(predictions, true_labels, probabilities, label_names)
+
 
 def main():
     args = parser.parse_args()
     config = who_wins_lib.read_config(args.config)
 
     for source in config.dev:
-        with open(f"results/{config.config_name}_dev.csv", "r") as f:
-            reader = csv.DictReader(f)
-            label_names = reader.fieldnames[3:]
-            rows = [r for r in reader]
-
-            predictions = [int(row['label']) for row in rows]
-            true_labels = [int(row['true_label']) for row in rows]
-            probabilities = [[float(row[name]) for name in label_names] for row in rows]
-            #true_class_probs = [
-            #  row[label_names[int(row['true_label'])]] for row in rows
-            #]
-            #top_class_probs = [
-            #  row[label_names[int(row['label'])]] for row in rows
-            #]
-
-    print(collections.Counter(predictions))
-    print(collections.Counter(true_labels))
-
-
-    #predictions = predictions[:10]
-    #true_labels = true_labels[:10]
-    #probabilities = probabilities[:10]
-
-
-
-    for label_i, label in enumerate(label_names):
-      new_labels = modify_to_binary(true_labels, label_i)
-      for confidence_x100 in range(0, 1000, 100):
-        confidence = confidence_x100 / 1000
-        print(label, confidence)
-        new_predictions = modify_with_confidence(probabilities, label_i, confidence)
-        print(new_labels)
-        print(new_predictions)
-        metrics = precision_recall_fscore_support(true_labels, predictions)
-        print(metrics[1][label_i])
-        #print(len(metrics), len(metrics[0]))
-        #print(metrics)
+        # You can also do this on train if you have run eval with the train set
+        result_file = read_result_file(f"results/{config.config_name}_dev.csv")
+        unused_confidence_stuff = """
+        for confidence_x100 in range(0, 1000, 100):
+            confidence = confidence_x100 / 1000
+            for label_i, label in enumerate(result_file.label_names):
+                new_labels = modify_to_binary(result_file.true_labels, label_i)
+                new_predictions = modify_with_confidence(
+                   result_file.probabilities, label_i, confidence
+                )
+                metrics = precision_recall_fscore_support(
+                  result_file.true_labels, result_file.predictions)
+                print(label, confidence, metrics)
+            print()
+        """
+        metrics = precision_recall_fscore_support(
+            result_file.true_labels, result_file.predictions
+        )
+        print(source, " ".join(result_file.label_names))
+        for name, metric in zip("precision recall fscore support".split(),
+        metrics):
+          print(name, metric)
         print()
-      print()
-
-
-    #for k, v in results.items():
-    #  for kk, vv in v.items():
-    #    print(k, kk, vv)
-    #  print()
-
 
 
 
