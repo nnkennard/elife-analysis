c1cb26303721d1550d063ed3f8391bd1414bb9f7
Daniel Scott Smith <danielscottsmith@stanford.edu>
2023-05-09 21:41:18 +0000

    parse results by source

--- who_wins/04_analyze_results.py
+++ who_wins/04_analyze_results.py
@@ -1,10 +1,12 @@
 import argparse
 import collections
 import csv
-
+import io
+import yaml
 import who_wins_lib
 
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+# from sklearn.metrics import f1_score
 
 parser = argparse.ArgumentParser(description="Analyze Who Wins results")
 parser.add_argument(
@@ -26,6 +28,21 @@ parser.add_argument(
     help="confidence threshold for filtering",
 )
 
+Config = collections.namedtuple(
+    "Config", "config_name task model_name train dev test predict labels".split()
+)
+
+
+def read_config(config_name, schema_path="schema.yml"):
+    with open(schema_path, "r") as f:
+        schema = yaml.safe_load(io.StringIO(f.read()))
+
+    with open(f"{CONFIG_PATH}/{config_name}.yml", "r") as f:
+        config = yaml.safe_load(io.StringIO(f.read()))
+        assert config["config_name"] == config_name
+        assert config["task"] in schema["tasks"]
+        return Config(labels=schema["labels"][config["task"]], **config)
+    
 Result = collections.namedtuple("Result", "precision recall fscore support".split())
 
 
@@ -51,17 +68,27 @@ ResultFile = collections.namedtuple(
 )
 
 
-def read_result_file(filename):
+# def read_result_file(filename):
+#     with open(filename, "r") as f:
+#         reader = csv.DictReader(f)
+#         label_names = reader.fieldnames[3:]
+#         rows = [r for r in reader]
+#     predictions = [int(row["label"]) for row in rows]
+#     true_labels = [int(row["true_label"]) for row in rows]
+#     probabilities = [[float(row[name]) for name in label_names] for row in rows]
+
+#     return ResultFile(predictions, true_labels, probabilities, label_names)
+
+def read_result_file(filename, source):
     with open(filename, "r") as f:
         reader = csv.DictReader(f)
         label_names = reader.fieldnames[3:]
         rows = [r for r in reader]
-    predictions = [int(row["label"]) for row in rows]
-    true_labels = [int(row["true_label"]) for row in rows]
-    probabilities = [[float(row[name]) for name in label_names] for row in rows]
+    predictions = [int(row["label"]) for row in rows if source in row['identifier']]
+    true_labels = [int(row["true_label"]) for row in rows if source in row['identifier']]
+    probabilities = [[float(row[name]) for name in label_names] for row in rows if source in row['identifier']]
 
     return ResultFile(predictions, true_labels, probabilities, label_names)
-
 
 def main():
     args = parser.parse_args()
@@ -69,7 +96,7 @@ def main():
 
     for source in config.dev:
         # You can also do this on train if you have run eval with the train set
-        result_file = read_result_file(f"results/{config.config_name}_dev.csv")
+        result_file = read_result_file(f"results/{config.config_name}_dev.csv", source)
         unused_confidence_stuff = """
         for confidence_x100 in range(0, 1000, 100):
             confidence = confidence_x100 / 1000
@@ -83,13 +110,14 @@ def main():
                 print(label, confidence, metrics)
             print()
         """
+        # metrics = precision_recall_fscore_support(
         metrics = precision_recall_fscore_support(
             result_file.true_labels, result_file.predictions
         )
         print(source, " ".join(result_file.label_names))
         for name, metric in zip("precision recall fscore support".split(),
         metrics):
-          print(name, metric)
+            print(name, metric)
         print()
 
 
